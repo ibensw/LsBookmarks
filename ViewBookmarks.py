650f5f6c33d0dc33376e0f3c1353217eefbaa466
Adam Zethraeus <adam@zethrae.us>
2020-07-28 01:25:10 -0700

    improve quick panel layout

--- ViewBookmarks.py
+++ ViewBookmarks.py
@@ -7,16 +7,20 @@ class ViewBookmarksCommand(sublime_plugin.WindowCommand):
     def run(self):
         items=[]
         self.locations=[]
+        all_files = list(filter(None, map(lambda f : f.file_name(), sublime.active_window().views())))
+        common_prefix = os.path.commonprefix(all_files)
         for view in sublime.active_window().views():
             prefix=""
             if view.name():
                 prefix=view.name()+":"
             elif view.file_name():
-                prefix=os.path.basename(view.file_name())+":"
+                prefix=view.file_name()+":"
+                if prefix.startswith(common_prefix):
+                    prefix = prefix[len(common_prefix):]
             for region in view.get_regions("bookmarks"):
                 row,_=view.rowcol(region.a)
                 line=re.sub('\s+', ' ', view.substr(view.line(region))).strip()
-                items.append(prefix+str(row+1)+": "+line)
+                items.append([prefix+str(row+1), line])
                 self.locations.append((view, region))
         if len(items) > 0:
             sublime.active_window().show_quick_panel(items, self.go_there, sublime.MONOSPACE_FONT)
@@ -31,3 +35,4 @@ class ViewBookmarksCommand(sublime_plugin.WindowCommand):
         view.show_at_center(region)
         view.sel().clear()
         view.sel().add(region)
+
